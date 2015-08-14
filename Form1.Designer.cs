a5519d0f6666234472f4992ae424aaddba9635b0
learningTests <osana@MPR-LPT-OS-01.mprise.group>
2015-08-14 18:44:32 +0200

    new proj

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace OrderGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.testTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.buttonMoveUp = new System.Windows.Forms.Button();
+            this.buttonMoveDown = new System.Windows.Forms.Button();
+            this.richTextBoxDetailsGrid = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridViewTestTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.testTableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTestTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // testTableBindingSource
+            // 
+            this.testTableBindingSource.DataMember = "TestTable";
+            // 
+            // buttonMoveUp
+            // 
+            this.buttonMoveUp.Location = new System.Drawing.Point(549, 86);
+            this.buttonMoveUp.Name = "buttonMoveUp";
+            this.buttonMoveUp.Size = new System.Drawing.Size(147, 23);
+            this.buttonMoveUp.TabIndex = 1;
+            this.buttonMoveUp.Text = "MoveUp";
+            this.buttonMoveUp.UseVisualStyleBackColor = true;
+            this.buttonMoveUp.Click += new System.EventHandler(this.buttonMoveUp_Click);
+            // 
+            // buttonMoveDown
+            // 
+            this.buttonMoveDown.Location = new System.Drawing.Point(549, 115);
+            this.buttonMoveDown.Name = "buttonMoveDown";
+            this.buttonMoveDown.Size = new System.Drawing.Size(147, 23);
+            this.buttonMoveDown.TabIndex = 2;
+            this.buttonMoveDown.Text = "ModeDown";
+            this.buttonMoveDown.UseVisualStyleBackColor = true;
+            this.buttonMoveDown.Click += new System.EventHandler(this.buttonMoveDown_Click);
+            // 
+            // richTextBoxDetailsGrid
+            // 
+            this.richTextBoxDetailsGrid.Location = new System.Drawing.Point(549, 186);
+            this.richTextBoxDetailsGrid.Name = "richTextBoxDetailsGrid";
+            this.richTextBoxDetailsGrid.Size = new System.Drawing.Size(279, 157);
+            this.richTextBoxDetailsGrid.TabIndex = 4;
+            this.richTextBoxDetailsGrid.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridViewTestTable
+            // 
+            this.dataGridViewTestTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTestTable.Location = new System.Drawing.Point(75, 86);
+            this.dataGridViewTestTable.Name = "dataGridViewTestTable";
+            this.dataGridViewTestTable.Size = new System.Drawing.Size(450, 257);
+            this.dataGridViewTestTable.TabIndex = 5;
+            this.dataGridViewTestTable.SelectionChanged += new System.EventHandler(this.dataGridViewTestTable_SelectionChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 386);
+            this.Controls.Add(this.dataGridViewTestTable);
+            this.Controls.Add(this.richTextBoxDetailsGrid);
+            this.Controls.Add(this.buttonMoveDown);
+            this.Controls.Add(this.buttonMoveUp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.testTableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTestTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource testTableBindingSource;
+        private System.Windows.Forms.Button buttonMoveUp;
+        private System.Windows.Forms.Button buttonMoveDown;
+        private System.Windows.Forms.RichTextBox richTextBoxDetailsGrid;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rowOrderDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridViewTestTable;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrderGrid
+{
+    public partial class Form1 : Form
+    {
+        DataTable dataTable;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.initDataGridView();
+        }
+
+        private void initDataGridView()
+        {
+            dataTable = GetTable();
+
+            this.dataGridViewTestTable.DataSource = null;
+            this.dataGridViewTestTable.DataSource = dataTable;
+            this.dataGridViewTestTable.Columns["CurrentOrder"].Visible = false;
+            this.dataGridViewTestTable.Sort(this.dataGridViewTestTable.Columns["CurrentOrder"], ListSortDirection.Ascending);
+        }
+
+        private void buttonMoveUp_Click(object sender, EventArgs e)
+        {
+            int currentValueId = (int)this.dataGridViewTestTable.CurrentRow.Cells["Id"].Value;
+            
+            if (currentValueId != 0)
+            {
+                this.moveUpDown(currentValueId, -1);
+                this.positionCursorOnSelected(currentValueId);
+            }
+        }
+
+        private void buttonMoveDown_Click(object sender, EventArgs e)
+        {
+            int currentValueId = (int)this.dataGridViewTestTable.CurrentRow.Cells["Id"].Value;
+
+            if (currentValueId != 0)
+            {
+                this.moveUpDown(currentValueId, 1);
+                this.positionCursorOnSelected(currentValueId);
+            }
+        }
+
+        private void moveUpDown(int idToMove, int posToMove) //-1 = one up, 1 = one down on the list
+        {
+            //update dataTable
+            DataRow rowToMove = dataTable.Rows
+                        .Cast<DataRow>()
+                        .Where(r => (int)r["Id"] == idToMove)
+                        .OrderByDescending(r => r["Id"])
+                        .First();
+
+            if (rowToMove != null)
+            {
+                //select and update the previous or next row
+                DataRow row = null;
+                switch (posToMove)
+                {
+                    case -1:
+                        row = dataTable.Rows
+                            .Cast<DataRow>()
+                            .Where(r => (int)r["CurrentOrder"] < (int)rowToMove["CurrentOrder"])
+                            .OrderByDescending(r => r["CurrentOrder"])
+                            .FirstOrDefault();
+                        break;
+
+                    case 1:
+                        row = dataTable.Rows
+                            .Cast<DataRow>()
+                            .Where(r => (int)r["CurrentOrder"] > (int)rowToMove["CurrentOrder"])
+                            .OrderBy(r => r["CurrentOrder"])
+                            .FirstOrDefault();
+                        break;
+                }
+
+                if (row != null)
+                {
+                    row.BeginEdit();
+                    row["CurrentOrder"] = -999;
+                    row.EndEdit();
+                    row.AcceptChanges();
+
+                    //move the current row up/down
+                    rowToMove.BeginEdit();
+                    rowToMove["CurrentOrder"] = (int)rowToMove["CurrentOrder"] + posToMove;
+                    rowToMove.EndEdit();
+                    rowToMove.AcceptChanges();
+
+                    row.BeginEdit();
+                    row["CurrentOrder"] = (int)rowToMove["CurrentOrder"] - posToMove;
+                    row.EndEdit();
+                    row.AcceptChanges();
+                }
+            }
+        }
+
+        private void positionCursorOnSelected(int idValue)
+        {
+            bool valueAllowUserToAddRows = this.dataGridViewTestTable.AllowUserToAddRows;
+
+            //temporary disable it
+            this.dataGridViewTestTable.AllowUserToAddRows = false;
+
+            DataGridViewRow row = this.dataGridViewTestTable.Rows
+                .Cast<DataGridViewRow>()
+                .Where(r => r.Cells["Id"].Value.Equals(idValue))
+                .OrderByDescending(r => r.Cells["Id"])
+                .FirstOrDefault();
+
+            if (row != null)
+            {
+                this.dataGridViewTestTable.ClearSelection();
+                this.dataGridViewTestTable.CurrentCell = this.dataGridViewTestTable.Rows[row.Index].Cells[0];
+                this.dataGridViewTestTable.Rows[row.Index].Selected = true;
+            }
+
+            this.dataGridViewTestTable.AllowUserToAddRows = valueAllowUserToAddRows;
+        }
+
+        static DataTable GetTable()
+        {
+            // Here we create a DataTable with four columns.
+            DataTable table = new DataTable();
+            table.Columns.Add("Id", typeof(int));
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("CurrentOrder", typeof(int));
+
+            // Here we add five DataRows.
+            table.Rows.Add(10, "Hydralazine", 1);
+            table.Rows.Add(21, "Combivent", 2);
+            table.Rows.Add(25, "Indocin", 3);
+            table.Rows.Add(50, "Enebrel", 4);
+            table.Rows.Add(100, "Dilantin", 5);
+
+            return table;            
+        
+        }
+
+
+        private void dataGridViewTestTable_SelectionChanged(object sender, EventArgs e)
+        {
+            richTextBoxDetailsGrid.Clear();
+
+            richTextBoxDetailsGrid.AppendText("Current row details:");
+
+            if (dataGridViewTestTable != null
+                && dataGridViewTestTable.CurrentRow != null)
+                richTextBoxDetailsGrid.AppendText(String.Format("Index: {0}, Id: {1}", dataGridViewTestTable.CurrentRow.Index, dataGridViewTestTable.CurrentRow.Cells["Id"].Value));
+
+
+        }
+
+    }
+}
